376b1a7ba426f66bd70a9dbcb2620c7886bf52ff
TxarliRoldn <roldanpinerocarlos@gmail.com>
2023-12-13 10:18:05 +0100

    forces require energy

--- ase_prueba/fireball.py
+++ ase_prueba/fireball.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from ase.calculators.calculator import (Calculator,
                                         PropertyNotImplementedError,
+                                        CalculatorSetupError,
                                         all_changes)
 
 class Fireball(Calculator):
@@ -16,6 +17,42 @@ class Fireball(Calculator):
         Calculator.__init__(self, **kwargs)
         self._loaded_fdata = [] # We keep track of loaded Fdata's
 
+    def _load_fdata(self):
+        print(f"Loading Fdata for atoms {self._num_unique}") # Example
+        self._loaded_fdata.append("fdata1.dat")
+
+    def calculate(self, atoms=None, properties=['energy'], system_changes=all_changes):
+        Calculator.calculate(self, atoms, properties, system_changes)
+            
+        # If the atoms change load needed Fdata and allocate memory
+        if 'numbers' in system_changes:
+            self.initialize(self.atoms)
+
+        # Check if energy needed
+        if 'energy' in properties:
+            # Computation energy
+            print("Computing energies") # Example
+            self.energies = np.random.randn(len(atoms))
+            self.energy = np.sum(self.energies)
+            
+            # Save energy
+            self.results['energy'] = self.energy
+            self.results['energies'] = self.energies
+            self.results['free_energy'] = self.energy
+
+        # Check if forces needed
+        if 'forces' in properties:
+            # Requisite energies
+            if 'energies' not in self.results:
+                raise CalculatorSetupError("Energies must be computed before forces")
+            
+            # Computation forces
+            print("Computing forces") # Example
+            self.forces = np.random.randn(len(atoms))
+
+            # Save forces
+            self.results['forces'] = self.forces
+
     def initialize(self, atoms):
         self.numbers = atoms.get_atomic_numbers()
         self._num_unique = np.unique(self.numbers)
@@ -23,34 +60,3 @@ class Fireball(Calculator):
 
         self.energies = np.empty(len(atoms))
         self.forces = np.empty((len(atoms), 3))
-
-
-    def _load_fdata(self):
-        print(f"Loading Fdata for atoms {self._num_unique}")
-        self._loaded_fdata.append("fdata1.dat")
-
-    def calculate(self, atoms=None, properties=['energy'], system_changes=all_changes):
-        Calculator.calculate(self, atoms, properties, system_changes)
-            
-        # If the atoms change or is the first time load needed Fdata and allocate memory
-        if 'numbers' in system_changes:
-            self.initialize(self.atoms)
-
-        # RIGHT NOW ASSUME ALWAYS COMPUTE ENERGY: MUST DISCUSS!!
-
-        # Computation energy
-        self.energies = np.random.randn(len(atoms)) # Example
-        self.energy = np.sum(self.energies)
-        
-        # Save energy
-        self.results['energy'] = self.energy
-        self.results['energies'] = self.energies
-        self.results['free_energy'] = self.energy
-
-        # Check if forces needed
-        if 'forces' in properties:
-            # Computation forces
-            self.forces = np.random.randn(len(atoms)) # Example
-
-            # Save forces
-            self.results['forces'] = self.forces
