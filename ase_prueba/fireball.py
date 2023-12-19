1dd3ab4f0d0c77a433fefa7d0f8da037207b5c6e
TxarliRoldn <roldanpinerocarlos@gmail.com>
2023-12-19 15:54:04 +0100

    if less atoms not load fdata again

--- ase_prueba/fireball.py
+++ ase_prueba/fireball.py
@@ -1,8 +1,7 @@
+import warnings
 import numpy as np
 
 from ase.calculators.calculator import (Calculator,
-                                        PropertyNotImplementedError,
-                                        CalculatorSetupError,
                                         all_changes)
 
 class Fireball(Calculator):
@@ -15,10 +14,11 @@ class Fireball(Calculator):
 
     def __init__(self, **kwargs):
         Calculator.__init__(self, **kwargs)
+        self._numbers = set() # Keep track of our atoms
         self._loaded_fdata = [] # We keep track of loaded Fdata's
 
-    def _load_fdata(self):
-        print(f"Loading Fdata for atoms {self._num_unique}") # Example
+    def _load_fdata(self, numbers):
+        print(f"Loading Fdata for atoms {numbers}") # Example
         self._loaded_fdata.append("fdata1.dat")
 
     def _calculate_energies(self, atoms):
@@ -61,9 +61,12 @@ class Fireball(Calculator):
             self._calculate_forces(atoms)
 
     def initialize(self, atoms):
-        self.numbers = atoms.get_atomic_numbers()
-        self._num_unique = np.unique(self.numbers)
-        self._load_fdata()
+        numbers = atoms.get_atomic_numbers()
+        
+        # Imposes Python >=3.10
+        if newnums := set(numbers) - self._numbers:
+            self._numbers = newnums
+            self._load_fdata(newnums)
 
         self.energies = np.empty(len(atoms))
         self.forces = np.empty((len(atoms), 3))
