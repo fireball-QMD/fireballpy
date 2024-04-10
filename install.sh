a208c10de3c762282f7eafb1d938e87683efce06
TxarliRoldn <roldanpinerocarlos@gmail.com>
2024-04-10 17:06:59 +0200

    compile with gfortran

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 here=$(pwd)
 rm -fr build
-mkdir build  
+mkdir build
 cd build
 cmake ..
 make
-f2py3 -m fireball -c ../src/libf2py.f90 --fcompiler='ifort' -I. libfireballpy.a --link-lapack_opt
+if command -v ifort 2>/dev/null; then
+	f2py3 -m fireball -c ../src/libf2py.f90 --fcompiler='ifort' -I. libfireballpy.a --link-lapack_opt
+else
+	f2py3 -m fireball -c ../src/libf2py.f90 -I. libfireballpy.a --link-lapack_opt
+fi
 cd $here
 
 cd test
-./test2.py
+./test2.py # Creo que esta linea solo funciona para Dani asi que dont´t worry
 cd ..
-
--- remake.sh
+++ remake.sh
@@ -1,11 +1,14 @@
 here=$(pwd)
 cd build
 make
-f2py3 -m fireball -c ../src/libf2py.f90 --fcompiler='ifort' -I. libfireballpy.a --link-lapack_opt
+if command -v ifort 2>/dev/null; then
+	f2py3 -m fireball -c ../src/libf2py.f90 --fcompiler='ifort' -I. libfireballpy.a --link-lapack_opt
+else
+	f2py3 -m fireball -c ../src/libf2py.f90 -I. libfireballpy.a --link-lapack_opt
+fi
 cd $here
 cd test
 #./dia.py
 #./test.py
-./test2.py
+./test2.py # Creo que esta linea solo funciona para Dani asi que dont´t worry
 cd ..
-
