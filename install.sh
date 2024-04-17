b8cb4ce153c677823812ab4ce507ac70dbc7a6f3
TxarliRoldn <roldanpinerocarlos@gmail.com>
2024-04-17 12:21:33 +0200

    use gfortran

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ mkdir build
 cd build
 cmake ..
 make
-if command -v ifort 2>/dev/null; then
-	f2py3 -m _fireball -c ../src/libf2py.f90 --fcompiler='ifort' -I. libfireballpy.a --link-lapack_opt
-else
-	f2py3 -m _fireball -c ../src/libf2py.f90 -I. libfireballpy.a --link-lapack_opt
-fi
+#if command -v ifort 2>/dev/null; then
+#	f2py3 -m _fireball -c ../src/libf2py.f90 --fcompiler='ifort' -I. libfireballpy.a --link-lapack_opt
+#else
+f2py3 -m _fireball -c ../src/libf2py.f90 -I. libfireballpy.a --link-lapack_opt
+#fi
 mv _fireball*.so ../fireballpy/
 cd $here
 
--- remake.sh
+++ remake.sh
@@ -1,11 +1,11 @@
 here=$(pwd)
 cd build
 make
-if command -v ifort 2>/dev/null; then
-	f2py3 -m _fireball -c ../src/libf2py.f90 --fcompiler='ifort' -I. libfireballpy.a --link-lapack_opt
-else
-	f2py3 -m _fireball -c ../src/libf2py.f90 -I. libfireballpy.a --link-lapack_opt
-fi
+#if command -v ifort 2>/dev/null; then
+#	f2py3 -m _fireball -c ../src/libf2py.f90 --fcompiler='ifort' -I. libfireballpy.a --link-lapack_opt
+#else
+f2py3 -m _fireball -c ../src/libf2py.f90 -I. libfireballpy.a --link-lapack_opt
+#fi
 mv _fireball*.so ../fireballpy/
 cd $here
 cd test
