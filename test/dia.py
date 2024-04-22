36dd4299711dbde9b3f39bc3d3a8febff8457f2b
dgtrabada <dgtrabada@gmail.com>
2024-04-22 14:42:03 +0200

    charges

--- test/dia.py
+++ test/dia.py
@@ -13,15 +13,20 @@ atoms = Atoms(symbols=['C', 'C'],
 atoms.set_cell([(a, a, 0), (a, 0, a), (0, a, a)])
 
 atoms.calc = Fireball(igamma=1, icluster=1, charges="Lowdin") 
-#,charges="Mulliken")
-# Mulliken-dipole
-# NPA:
-# Lowdin
-# Mulliken-dipole-preserving
 
 ETOT = atoms.get_potential_energy()
 print("ETOT = "+str(ETOT))
 
+
+charge = atoms.get_charges()
+print("------atoms.charges-----------")
+for c in charge:
+    print(c)
+
+force = atoms.get_forces()
+print("------atoms.forces------------")
+for f in force:
+    print(f)
 
 
 
