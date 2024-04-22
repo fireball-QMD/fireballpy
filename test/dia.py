5871ff342857f997de3f5ab2429e2b2dc378876f
dgtrabada <dgtrabada@gmail.com>
2024-04-22 10:17:07 +0200

    lvs

--- test/dia.py
+++ test/dia.py
@@ -1,30 +1,21 @@
-#!/usr/bin/env python
-
 import sys
 import os
-sys.path.append("/home/dani/fireballpy/build")
-import fireballpy as fb
+sys.path.append("..")
+from fireballpy import Fireball
+from ase import Atoms
 
-fb.loadfdata_from_path("/home/dani/Fdata_HC-new/")
-fb.info_fdata()
 
-fb.loadbas_from_file("/home/dani/fireballpy/test/dia2.bas")
-fb.loadlvs_from_file("/home/dani/fireballpy/test/dia2.lvs")
-fb.loadkpts_from_file("/home/dani/fireballpy/test/dia2.kpts")
-fb.set_icluster(0)
-fb.set_idipole(0)
-fb.set_gamma(0)
-fb.set_iqout(7)
-fb.rescal_structure(3.569)
-#fb.print_atoms_positions()
-fb.call_allocate_system()
-fb.call_scf_loop()
-fb.call_getenergy()
-print('  ==== CHARGES =====')
-fb.info_charges()
-fb.info_energy()
-fb.call_getforces()
-fb.info_forces()
+a=3.569
+atoms = Atoms(symbols=['C', 'C'],
+            positions=[( 0.0000, 0.0000, 0.0000),
+                       (    a/2,    a/2,    a/2)])
+
+atoms.set_cell([(a, a, 0), (a, 0, a), (0, a, a)])
+
+atoms.calc = Fireball(igamma=1, icluster=1)
+ETOT = atoms.get_potential_energy()
+print("ETOT = "+str(ETOT))
+
 
 
 
