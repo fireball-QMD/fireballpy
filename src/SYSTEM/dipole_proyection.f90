0e87a09f03c8eed602f3fe9773d27edcb2f2bcad
dgtrabada <dgtrabada@gmail.com>
2024-04-29 12:38:46 +0200

    7ok

--- src/SYSTEM/dipole_proyection.f90
+++ src/SYSTEM/dipole_proyection.f90
@@ -22,10 +22,13 @@ subroutine dipole_proyection()
   real*8, dimension (neigh_max) :: w_k
   real*8, dimension(3,natoms) :: intra_dip, res_dip
   real*8, dimension(3,1) :: intra_dip_aux, delta_ck
-  integer n_bwrr, lda_bwrr, info, lda, i, lwork
+  integer :: n_bwrr = 3
+  integer :: lda_bwrr = 3
+  integer :: lwork = 15 !MAX(1,3*MIN(M,N) + MAX(M,N),5*MIN(M,N)) 
+  integer info,i
   integer, dimension(3) :: ipiv
   real*8, dimension(3) :: s_bwrr
-  real*8,  allocatable, dimension (:) :: dummy
+  real*8, dimension(15) :: dummy
   do iatom = 1, natoms
     Q0_TOT(iatom) = 0
     in1 = imass(iatom)
@@ -219,18 +222,8 @@ subroutine dipole_proyection()
       enddo !imu
     end do !end ineigh = 1,natoms
     !inversa de bwrr
-    n_bwrr = 3
-    lda_bwrr = n_bwrr
-    lwork = n_bwrr
     bwrr_inv = bwrr
-    lwork = 1
-    allocate (dummy(lwork))
-    call dgesvd('A', 'S', n_bwrr, n_bwrr, bwrr_inv, lda_bwrr, s_bwrr, u_bwrr, lda_bwrr, vt_bwrr, lda_bwrr, dummy, -1, info)
-    lwork = dummy(1)
-    deallocate(dummy)
-    allocate (dummy(lwork))
     call dgesvd('A', 'S', n_bwrr, n_bwrr, bwrr_inv, lda_bwrr, s_bwrr, u_bwrr, lda_bwrr, vt_bwrr, lda_bwrr, dummy, lwork, info)
-    deallocate(dummy)
     zero_bwrr = 0.00
     do i = 1,3
       if (abs(s_bwrr(i)) .gt. 0.000001) then
